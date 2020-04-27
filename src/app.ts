a5bfb83057b2bb504c1017a39c5f46423b87d68f
koki <myaddressisthis123@gmail.com>
2020-04-27 22:33:03 +0900

    feat: socket通信導通

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import path from 'path';
 import { testQuery } from '@/repository/test';
+import * as http from "http";
+import socketIO from "socket.io";
+
+const PORT = process.env.PORT || 7000;
 
 const app = express();
-const PORT = process.env.PORT || 7000;
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", (socket: socketIO.Socket) => {
+    socket.on("create_game_table", (msg: string) => {
+        io.emit("created_game_table", msg);
+    });
+});
 
 app.get('/', function (req: express.Request, res: express.Response) {
     res.sendFile(path.resolve('index.html'));
@@ -12,6 +23,7 @@ app.get('/db', function (req: express.Request, res: express.Response) {
     testQuery()
     res.status(200).send('ok')
 });
-app.listen(PORT, function () {
+
+server.listen(PORT, function () {
     console.log('server listening. Port:' + PORT);
 });
