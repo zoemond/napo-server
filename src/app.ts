69de6a00596a9e45461fa0c6f73dbe890066d355
koki <myaddressisthis123@gmail.com>
2020-04-28 09:48:37 +0900

    style: format app.ts by prettier

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { testQuery } from '@/repository/test';
+import express from "express";
+import path from "path";
+import { testQuery } from "@/repository/test";
 import * as http from "http";
 import socketIO from "socket.io";
 
@@ -11,19 +11,19 @@ const server = http.createServer(app);
 const io = socketIO(server);
 
 io.on("connection", (socket: socketIO.Socket) => {
-    socket.on("create_game_table", (msg: string) => {
-        io.emit("created_game_table", msg);
-    });
+  socket.on("create_game_table", (msg: string) => {
+    io.emit("created_game_table", msg);
+  });
 });
 
-app.get('/', function (req: express.Request, res: express.Response) {
-    res.sendFile(path.resolve('index.html'));
+app.get("/", function (req: express.Request, res: express.Response) {
+  res.sendFile(path.resolve("index.html"));
 });
-app.get('/db', function (req: express.Request, res: express.Response) {
-    testQuery()
-    res.status(200).send('ok')
+app.get("/db", function (req: express.Request, res: express.Response) {
+  testQuery();
+  res.status(200).send("ok");
 });
 
 server.listen(PORT, function () {
-    console.log('server listening. Port:' + PORT);
+  console.log("server listening. Port:" + PORT);
 });
