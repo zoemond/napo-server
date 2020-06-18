ec45e25bb959aa5aca17590b940b0e112bece1b3
zoemond <dummy@example.com>
2020-06-19 05:29:03 +0900

    refactor: WebSocket通信とは関係ない処理をeventsに分離: declareTrump

--- src/presentation/events/declaration_events.ts
+++ src/presentation/events/declaration_events.ts
@@ -1,7 +1,13 @@
 import { DeclarationResponse } from "../response/DeclarationResponse";
 import DeclarationRepository from "~/repository/DeclarationRepository";
+import GameCardsRepository from "~/repository/GameCardsRepository";
+import { Trump } from "~/domain/Trump";
+import Card from "~/domain/Card";
+import { SeatName } from "~/domain/SeatName";
+import { Declaration } from "~/domain/Declaration";
 
 const declarationRepository = new DeclarationRepository();
+const seatsRepository = new GameCardsRepository();
 
 export async function getDeclaration(
   gameTableId: number,
@@ -11,6 +17,57 @@ export async function getDeclaration(
     const declaration = await declarationRepository.getDeclaration(
       gameTableId,
       roundCount
+    );
+    return { gameTableId, declaration };
+  } catch (error) {
+    return { errorMessage: error.message };
+  }
+}
+
+export async function declareTrump(
+  gameTableId: number,
+  trump: Trump,
+  faceCardNumber: number,
+  aideCard: Card,
+  napoleon: SeatName,
+  discards: [Card, Card]
+): Promise<DeclarationResponse> {
+  try {
+    const [discard1, discard2] = discards;
+    if (discard1.equals(discard2)) {
+      throw new Error("同じカードは捨てられません.");
+    }
+
+    const seats = await seatsRepository.getSeats(gameTableId);
+    const round = await seatsRepository.getRound(gameTableId);
+    const hands = seats
+      .find((seat) => seat.seatName === napoleon)
+      ?.hands.concat(round.openCards as [Card, Card])
+      .filter((card) => !card.equals(discard1) && !card.equals(discard2));
+    console.log("hands", hands);
+    console.log("opens", discards);
+    if (!hands) {
+      throw new Error(
+        `ナポレオンを特定できませんでした. [napoleon: ${napoleon}]`
+      );
+    }
+
+    await declarationRepository.declareNapoleon(
+      gameTableId,
+      round.roundCount,
+      faceCardNumber,
+      trump,
+      napoleon,
+      aideCard,
+      discards
+    );
+    await seatsRepository.handOutSeat(gameTableId, napoleon, hands);
+    const declaration = new Declaration(
+      faceCardNumber,
+      trump,
+      napoleon,
+      aideCard,
+      discards
     );
     return { gameTableId, declaration };
   } catch (error) {
--- src/presentation/socket_events/declaration_events.ts
+++ src/presentation/socket_events/declaration_events.ts
@@ -1,66 +1,7 @@
 import socketIO from "socket.io";
-import GameCardsRepository from "~/repository/GameCardsRepository";
 import Card from "~/domain/Card";
-import { SeatName } from "~/domain/SeatName";
-import DeclarationRepository from "~/repository/DeclarationRepository";
-import { DeclarationResponse } from "../response/DeclarationResponse";
-import { Trump } from "~/domain/Trump";
-import { Declaration } from "~/domain/Declaration";
 import { readSeats } from "../events/seats_events";
-
-const declarationRepository = new DeclarationRepository();
-const seatsRepository = new GameCardsRepository();
-
-async function declareTrump(
-  gameTableId: number,
-  trump: Trump,
-  faceCardNumber: number,
-  aideCard: Card,
-  napoleon: SeatName,
-  discards: [Card, Card]
-): Promise<DeclarationResponse> {
-  try {
-    const [discard1, discard2] = discards;
-    if (discard1.equals(discard2)) {
-      throw new Error("同じカードは捨てられません.");
-    }
-
-    const seats = await seatsRepository.getSeats(gameTableId);
-    const round = await seatsRepository.getRound(gameTableId);
-    const hands = seats
-      .find((seat) => seat.seatName === napoleon)
-      ?.hands.concat(round.openCards as [Card, Card])
-      .filter((card) => !card.equals(discard1) && !card.equals(discard2));
-    console.log("hands", hands);
-    console.log("opens", discards);
-    if (!hands) {
-      throw new Error(
-        `ナポレオンを特定できませんでした. [napoleon: ${napoleon}]`
-      );
-    }
-
-    await declarationRepository.declareNapoleon(
-      gameTableId,
-      round.roundCount,
-      faceCardNumber,
-      trump,
-      napoleon,
-      aideCard,
-      discards
-    );
-    await seatsRepository.handOutSeat(gameTableId, napoleon, hands);
-    const declaration = new Declaration(
-      faceCardNumber,
-      trump,
-      napoleon,
-      aideCard,
-      discards
-    );
-    return { gameTableId, declaration };
-  } catch (error) {
-    return { errorMessage: error.message };
-  }
-}
+import { declareTrump } from "../events/declaration_events";
 
 export function setDeclareTrumpEvent(
   socket: socketIO.Socket,
