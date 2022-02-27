d4785d67e3e13104d1382a68808328729ee72d9e
Kohei Hosokawa <teledx72@gmail.com>
2022-02-27 18:42:43 +0900

    :fire:

--- tools/init_db.sql
+++
@@ -1,5 +0,0 @@
-\i ../docker/psql/initdb.d/00_init.sql
-\i ../docker/psql/initdb.d/create_seats_table.sql
-\i ../docker/psql/initdb.d/create_rounds_table.sql
-\i ../docker/psql/initdb.d/create_declarations_table.sql
-\i ../docker/psql/initdb.d/10_create_game_tables_table.sql
